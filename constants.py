28c3985b6acc24afc7da1c773dcf7b3ce80dc5dc
crestel-ong <crestel.ong@mths.ca>
2021-09-19 21:16:48 +0000

    good

---
+++ constants.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+# Created by: Crestel Ong
+# Created on: Sept 2021
+# This module contains a constant needed to find the perimiter of a hexagon
+
+NUMBER_OF_SIDES = 6
--- perimeter_of_hexagon.py
+++ perimeter_of_hexagon.py
@@ -5,15 +5,19 @@
 # This programs calculates the perimeter of a hexagon
 #     user inputs the length of one side
 
+import constants
+
 
 def main():
-    # this function calculates the perimeter of a hexagon
+    # main function
+    print("We will be calculating the perimiter of a hexagon.")
 
     # input
-    side_length = int(input("Enter the length of one side of the hexagon (cm) : "))
+    print("")
+    side_length = int(input("Enter the length of one side (cm) : "))
 
     # process
-    perimeter = 6 * side_length
+    perimeter = constants.NUMBER_OF_SIDES * side_length
 
     # output
     print("The perimeter of this hexagon is {0} cm.".format(perimeter))
